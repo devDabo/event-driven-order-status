65dc452774b854611bd5d5a4e7543b57e946580f
c1673142 <raj.chand.99@icloud.com>
2025-11-15 14:42:44 +0000

    init commit

---
+++ order-status-consumer/src/main/java/com/training/order/consumer/OrderStatusConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.training.order.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderStatusConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderStatusConsumerApplication.class, args);
+	}
+
+}
---
+++ order-status-producer/src/main/java/com/training/order/producer/OrderStatusProducerApplication.java
@@ -0,0 +1,13 @@
+package com.training.order.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderStatusProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderStatusProducerApplication.class, args);
+	}
+
+}
